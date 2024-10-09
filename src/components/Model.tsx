7ba39d2555877eb6c5e45e16b5fad101f7c412ce
cforChobar <sidhuashman02@gmail.com>
2024-10-09 19:15:38 +0530

    created 3d iphone model and lights and cameras

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -6,10 +6,17 @@ import { yellowImg } from "../utils"
 import * as THREE from "three"
 import { Canvas } from "@react-three/fiber"
 import { View } from "@react-three/drei"
+import { models, sizes } from "../constants"
+
+export interface ModelObject {
+    title : string
+    color : string[]
+    img : string
+}
 
 const Model = () => {
     const [size, setSize] = useState("small")
-    const [model, setModel] = useState({
+    const [model, setModel] = useState<ModelObject>({
         title : "iPhone 15 Pro in Natural Titanium",
         color : ["#8F8A81", "#FFE7B9", "#6F6C64"],
         img : yellowImg
@@ -43,7 +50,7 @@ const Model = () => {
                 groupRef={small}
                 gsapType="view1"
                 controlRef={cameraControlSmall}
-                setRotationState={setSmallRotation}
+                setRotationSize={setSmallRotation}
                 item={model}
                 size={size}
                 />
@@ -52,7 +59,7 @@ const Model = () => {
                 groupRef={large}
                 gsapType="view2"
                 controlRef={cameraControlLarge}
-                setRotationState={setLargeRotation}
+                setRotationSize={setLargeRotation}
                 item={model}
                 size={size}
                 />
@@ -60,6 +67,24 @@ const Model = () => {
                 <Canvas className="w-full h-full" style={{position : "fixed", top : 0, bottom : 0, left : 0, right : 0, overflow : "hidden"}} eventSource={document.getElementById('root')}>
                     <View.Port/>
                 </Canvas>
+            </div>
+            <div className="mx-auto w-full">
+                <p className="text-sm font-light text-center mb-5">{model.title}</p>
+                <div className="flex-center">
+                    <ul className="color-container">
+                        {models.map((item, i) => (
+                            <li key={i} className="w-6 h-6 rounded-full mx-2 cursor-pointer" style={{backgroundColor : item.color[0]}} onClick={() => setModel(item)}></li>
+                        ))}
+                    </ul>
+                        <button className="size-btn-container">
+                            {sizes.map(({label, value}) => (
+                                <span key={label} className="size-btn" style={{backgroundColor : size === value ? "white" : "transparent", color : size === value ? "black" : "white"}} onClick={() => setSize(value)}>
+                                    {label}
+                                </span>
+                            ))}
+                        </button>
+                </div>
+
             </div>
         </div>
     </div>
--- src/components/ModelView.tsx
+++ src/components/ModelView.tsx
@@ -1,8 +1,39 @@
-const ModelView = () => {
+import { Dispatch, MutableRefObject, SetStateAction, Suspense } from "react"
+import * as THREE from "three"
+import { ModelObject } from "./Model"
+import { Html, PerspectiveCamera, View } from "@react-three/drei"
+import Lights from "./Lights"
+import IPhone from "./IPhone"
+
+
+interface ModelViewProps {
+  index : number
+  groupRef : MutableRefObject<THREE.Group<THREE.Object3DEventMap>>
+  gsapType : string
+  controlRef : MutableRefObject<undefined>
+  setRotationSize : Dispatch<SetStateAction<number>>
+  item : ModelObject
+  size : string
+}
+
+
+const ModelView = ({index, groupRef, gsapType, controlRef, setRotationSize, item, size} : ModelViewProps) => {
   return (
-    <div>
-      Model View
-    </div>
+    <View
+    index={index}
+    id={gsapType}
+    className={`w-full h-full ${index === 2 ? 'right-[-100%]' : ''}`}
+    >
+      {/* Ambient Light */}
+      <ambientLight intensity={0.3} />
+      <PerspectiveCamera makeDefault position={[0,0,4]} />
+
+      <Lights/>
+
+      <Suspense fallback={<Html><div>Loading...</div></Html>}>
+        <IPhone/>
+      </Suspense>
+    </View>
   )
 }
 
