d9473d081207e36b97b858e82d46f3276cd7badc
cforChobar <sidhuashman02@gmail.com>
2024-10-10 13:11:55 +0530

    added 3D apple model

--- src/components/ModelView.tsx
+++ src/components/ModelView.tsx
@@ -1,38 +1,73 @@
-import { Dispatch, MutableRefObject, SetStateAction, Suspense } from "react"
+import {
+  Dispatch,
+  MutableRefObject,
+  SetStateAction,
+  Suspense,
+} from "react"
 import * as THREE from "three"
 import { ModelObject } from "./Model"
-import { Html, PerspectiveCamera, View } from "@react-three/drei"
+import {  PerspectiveCamera, View, OrbitControls } from "@react-three/drei"
 import Lights from "./Lights"
 import IPhone from "./IPhone"
-
+import Loader from "./Loader"
 
 interface ModelViewProps {
-  index : number
-  groupRef : MutableRefObject<THREE.Group<THREE.Object3DEventMap>>
-  gsapType : string
-  controlRef : MutableRefObject<undefined>
-  setRotationSize : Dispatch<SetStateAction<number>>
-  item : ModelObject
-  size : string
+  index: number
+  groupRef: MutableRefObject<THREE.Group<THREE.Object3DEventMap>>
+  gsapType: string
+  controlRef: any
+  setRotationState: Dispatch<SetStateAction<number>>
+  item: ModelObject
+  size: string
 }
 
-
-const ModelView = ({index, groupRef, gsapType, controlRef, setRotationSize, item, size} : ModelViewProps) => {
+const ModelView = ({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationState,
+  item,
+  size,
+}: ModelViewProps) => {
   return (
     <View
-    index={index}
-    id={gsapType}
-    className={`w-full h-full ${index === 2 ? 'right-[-100%]' : ''}`}
+      index={index}
+      id={gsapType}
+      className={`w-full h-full absolute ${index === 2 ? "right-[-100%]" : ""}`}
     >
       {/* Ambient Light */}
-      <ambientLight intensity={0.3} />
-      <PerspectiveCamera makeDefault position={[0,0,4]} />
+      <ambientLight intensity={.5} />
+      <PerspectiveCamera makeDefault position={[0, 0, 5]} />
 
-      <Lights/>
+      <Lights />
 
-      <Suspense fallback={<Html><div>Loading...</div></Html>}>
-        <IPhone/>
-      </Suspense>
+      <OrbitControls
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0, 0)}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+      />
+      <group
+        ref={groupRef}
+        name={index === 1 ? "small" : "large"}
+        position={[0, 0, 0]}
+      >
+        <Suspense
+          fallback={
+           <Loader/>
+          }
+        >
+          <IPhone
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            item={item}
+            size={size}
+          />
+        </Suspense>
+      </group>
     </View>
   )
 }
