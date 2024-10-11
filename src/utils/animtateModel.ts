56660c28eceb263be70750d5bc9daf35b4d6af37
cforChobar <sidhuashman02@gmail.com>
2024-10-11 12:28:20 +0530

    features section is added

--- src/utils/animtateModel.ts
+++ src/utils/animtateModel.ts
@@ -1,4 +1,20 @@
 import * as THREE from "three"
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/all"
+
+gsap.registerPlugin(ScrollTrigger)
+
+export const animateWithGsap = (target : string, animationProps : {[key : string] : string | number}, scrollProps? : {[key : string] : string | number}) => {
+    gsap.to(target, {
+        ...animationProps,
+        scrollTrigger : {
+            trigger : target,
+            toggleActions : "restart reverse restart reverse",
+            start : "top 85%",
+            ...scrollProps
+        }
+    })
+}
 
 export const animateWithGsapTimeline = (timeline : gsap.core.Timeline, rotationRef : React.MutableRefObject<THREE.Group<THREE.Object3DEventMap>>, rotationState : number, firstTarget : string, secondTarget : string, animationProps : {
     [key : string] : string | number
